5c2d1fda0f2d886e4402e0731b98af540e3f7039
GenWatt <adrian.kosoglos@gmail.com>
2024-09-11 18:15:06 +0200

    Update selected details for building update

--- Assets/Scripts/UI/SelectedDetails/BuildingUpdater.cs
+++ Assets/Scripts/UI/SelectedDetails/BuildingUpdater.cs
@@ -9,8 +9,20 @@ public class BuildingDetailsUpdater
     private Label levelText;
     private VisualElement actions;
     private UIStorage uIStorage;
+    private Button levelUpButton;
+    private Button sellButton;
+    private VisualElement attackActions;
 
-    public BuildingDetailsUpdater(VisualElement statsContainer, ProgressBar healthBar, ProgressBar expirenceBar, Label levelText, VisualElement actions, UIStorage uIStorage)
+    public BuildingDetailsUpdater(
+        VisualElement statsContainer,
+        ProgressBar healthBar,
+        ProgressBar expirenceBar,
+        Label levelText,
+        VisualElement actions,
+        UIStorage uIStorage,
+        Button levelUpButton,
+        Button sellButton,
+        VisualElement attackActions)
     {
         this.statsContainer = statsContainer;
         this.healthBar = healthBar;
@@ -18,6 +30,9 @@ public class BuildingDetailsUpdater
         this.levelText = levelText;
         this.actions = actions;
         this.uIStorage = uIStorage;
+        this.levelUpButton = levelUpButton;
+        this.sellButton = sellButton;
+        this.attackActions = attackActions;
     }
 
     public void UpdateBuildingDetails(Selectable selectable)
@@ -25,6 +40,7 @@ public class BuildingDetailsUpdater
         actions.style.display = DisplayStyle.Flex;
         var damagable = selectable.GetComponent<Damagable>();
         var building = selectable.GetComponent<Building>();
+        var construction = selectable.GetComponent<Construction>();
 
         if (damagable != null && building != null)
         {
@@ -35,7 +51,36 @@ public class BuildingDetailsUpdater
             UpdateHealthBar(health, maxHealth);
             HandleLeveling(building, damagable);
             ActivateBuildingCamera(damagable);
+
+            if (damagable.damagableSo.canAttack)
+            {
+                ShowHideAttackActions(true);
+            }
+            else
+            {
+                ShowHideAttackActions(false);
+            }
+
+            if (
+                construction != null ||
+                (building.buildingLevelable != null && building.buildingLevelable.maxLevel <= building.buildingLevelable.level.Value))
+            {
+                // construction (hide level up button)
+                levelUpButton.style.display = DisplayStyle.None;
+                sellButton.style.display = DisplayStyle.Flex;
+            }
+            else
+            {
+                // building (show level up button)
+                levelUpButton.style.display = DisplayStyle.Flex;
+                sellButton.style.display = DisplayStyle.Flex;
+            }
         }
+    }
+
+    private void ShowHideAttackActions(bool show)
+    {
+        attackActions.style.display = show ? DisplayStyle.Flex : DisplayStyle.None;
     }
 
     private void HandleLeveling(Building building, Damagable damagable)
@@ -43,15 +88,30 @@ public class BuildingDetailsUpdater
         // Handle level and experience display logic here
         if (building.buildingLevelable != null)
         {
-            var nextLevel = building.buildingLevelable.GetNextBuildingLevel();
-            if (nextLevel != null && uIStorage.HasEnoughResource(nextLevel.resourceSO, nextLevel.cost))
+            if (building.buildingLevelable.maxLevel > building.buildingLevelable.level.Value)
             {
+                // show level and activate buttons
                 levelText.text = $"{building.buildingLevelable.level.Value} LVL";
             }
             else
             {
+                // max level
                 levelText.text = $"MAX {building.buildingLevelable.level.Value} LVL";
             }
+
+            // check if enugh resources to level up
+            var nextBuildingLevel = building.buildingLevelable.GetNextBuildingLevel();
+
+            if (nextBuildingLevel != null && uIStorage.HasEnoughResource(nextBuildingLevel.resourceSO, nextBuildingLevel.cost))
+            {
+                levelUpButton.SetEnabled(true);
+            }
+            else
+            {
+                levelUpButton.SetEnabled(false);
+            }
+
+            UpdateExpirenceBar();
         }
     }
 
@@ -61,6 +121,11 @@ public class BuildingDetailsUpdater
         healthBar.value = health;
         healthBar.highValue = maxHealth;
         healthBar.title = $"HP: {health}/{maxHealth}";
+    }
+
+    private void UpdateExpirenceBar()
+    {
+        expirenceBar.style.display = DisplayStyle.None;
     }
 
     private void ActivateBuildingCamera(Damagable damagable)
