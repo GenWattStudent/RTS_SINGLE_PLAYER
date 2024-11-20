e9e95a4679151e3418ef225e4176333d9a2d76fa
GenWatt <adrian.kosoglos@gmail.com>
2024-11-20 21:57:11 +0100

    add power ups 3d models

---
+++ Assets/PowerUp/Scripts/TakeablePowerUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PowerUpLib
+{
+	public class TakeablePowerUp : MonoBehaviour
+	{
+		CustomizablePowerUp customPowerUp;
+
+		void Start()
+		{
+			customPowerUp = (CustomizablePowerUp)transform.parent.gameObject.GetComponent<CustomizablePowerUp>();
+			//this.audio.clip = customPowerUp.pickUpSound;
+		}
+
+		void OnTriggerEnter(Collider collider)
+		{
+			if (collider.tag == "Player")
+			{
+				PowerUpManager.Instance.Add(customPowerUp);
+				if (customPowerUp.pickUpSound != null)
+				{
+					AudioSource.PlayClipAtPoint(customPowerUp.pickUpSound, transform.position);
+				}
+				Destroy(transform.parent.gameObject);
+			}
+		}
+	}
+
+}
+
